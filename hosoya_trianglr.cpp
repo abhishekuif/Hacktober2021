cdb1a6ccbf413f9d484915f5b793b9bf0130ad75
Abhishek Kumar Singh <92250574+abhishekuif@users.noreply.github.com>
2021-10-11 08:00:22 -0700

    Update hosoya_trianglr.cpp

--- hosoya_trianglr.cpp
+++ hosoya_trianglr.cpp
@@ -43,3 +43,58 @@ int main()
 	printHosoya(n);
 	return 0;
 }
+// CPP Program to print Hosoya's
+
+// C# Program to print Hosoya's
+// triangle of height n.
+using System;
+ 
+class GFG {
+     
+    static int Hosoya(int n, int m)
+    {
+        // Base case
+        if ((n == 0 && m == 0) ||
+            (n == 1 && m == 0) ||
+            (n == 1 && m == 1) ||
+            (n == 2 && m == 1))
+            return 1;
+     
+        // Recursive step
+        if (n > m)
+            return Hosoya(n - 1, m)
+                 + Hosoya(n - 2, m);
+             
+        else if (m == n)
+            return Hosoya(n - 1, m - 1)
+                 + Hosoya(n - 2, m - 2);
+             
+        else
+            return 0;
+    }
+     
+    // Print the Hosoya triangle of height n.
+    static void printHosoya(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                Console.Write(Hosoya(i, j)
+                                        + " ");
+     
+            Console.WriteLine("");
+        }
+    }
+ 
+     
+    /* Driver program to test above function */
+    public static void Main()
+    {
+        int n = 5;
+         
+        printHosoya(n);
+         
+    }
+}
+ 
+// This code is contributed by vt_m.
